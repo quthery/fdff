5ff388462fe41f0f475e504914fc6b7a536325a0
quthery <chiefkeef7317@gmail.com>
2024-05-10 14:23:04 +0300

    1

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	otvet := "losharar"
+	massive := []string{"egg", "tea", "f"}
+	println("Привет что ты ел на завтрак?")
+	fmt.Scan(&otvet)
+
+	found := slices.Contains(massive, otvet)
+	if found == true {
+		println("Я угадал!")
+	} else {
+		println("Я не угадал!")
+	}
+
+}
